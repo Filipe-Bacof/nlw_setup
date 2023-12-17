b6a34b7073e616f66c9c4a1486c479ca1cf75e6b
Filipe Bacof <filipebacof@gmail.com>
2023-12-17 17:08:22 -0300

    feat: sem o dayjs

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -42,13 +42,15 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { date } = getDayParams.parse(request.query);
 
-    const parsedDate = dayjs(date).startOf("day");
-    const weekDay = parsedDate.get("day");
+    const parsedDate = new Date(date);
+    parsedDate.setHours(0, 0, 0, 0);
+
+    const weekDay = parsedDate.getDay();
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
         created_at: {
-          lte: date,
+          lte: parsedDate,
         },
         weekDays: {
           some: {
@@ -60,7 +62,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const day = await prisma.day.findUnique({
       where: {
-        date: parsedDate.toDate(),
+        date: parsedDate,
       },
       include: {
         dayHabits: true,
